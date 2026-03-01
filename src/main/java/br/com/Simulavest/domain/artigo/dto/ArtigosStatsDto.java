8cdcf9ad6bad4cb421138ebd63fd913d5d8c5bee
MagalDevs <danielgalfla12@gmail.com>
2026-03-01 15:38:20 -0300

    atualizando os dtos de artigo - traduzido para inglês

--- src/main/java/br/com/Simulavest/domain/artigo/dto/ArtigosStatsDto.java
+++ src/main/java/br/com/Simulavest/domain/artigo/dto/ArtigosStatsDto.java
@@ -1,11 +1,16 @@
 package br.com.Simulavest.domain.artigo.dto;
 
 import br.com.Simulavest.domain.artigo.ArtigoStats;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record ArtigosStatsDto(
+        @JsonProperty("likes")
         int curtidas,
+        @JsonProperty("views")
         int visualizacoes,
+        @JsonProperty("shares")
         int compartilhamentos,
+        @JsonProperty("readingTime")
         String tempoMedioLeitura
 ) {
     public ArtigosStatsDto(ArtigoStats artigoStats) {
